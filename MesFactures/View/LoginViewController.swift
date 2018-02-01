dd1958f22f9286f5c9d582ba54e19e46dd4a48f1
Sebastien Constant <constant.sebastien@gmail.com>
2018-02-01 15:07:40 +0100

    Initialisation
    Commit d’initialisation du projet.
    Exclusion du répertoire et des fichiers « pods »

---
+++ MesFactures/View/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MesFactures
+//
+//  Created by Sébastien on 01/02/2018.
+//  Copyright © 2018 Sébastien Constant. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
