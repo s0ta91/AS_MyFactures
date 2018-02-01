ecc6a58a07d120e9063a3a8b3781070f172272ee
Sebastien Constant <constant.sebastien@gmail.com>
2018-02-01 19:13:47 +0100

    Create login screen
    ** LoginViewController **
    
    - Création de l’écran (Storyboard)
    - Adaptation du clavier en fonction de l’affichage des textField (pour ne pas les cacher)
    - Adaptation de l’écran en fonction des différentes classes d’écran (iPad/iPhone plus/iPhone/iphoneSE-4S)
    - Ajout des assets (Couleur de texte et bouton / spécifiques fonts)

--- MesFactures/View/LoginViewController.swift
+++ MesFactures/View/LoginViewController.swift
@@ -7,13 +7,35 @@
 //
 
 import UIKit
+import IQKeyboardManagerSwift
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var mesfacturesTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        IQKeyboardManager.sharedManager().enable = true
+        IQKeyboardManager.sharedManager().enableAutoToolbar = false
+        mesfacturesTextField.font = UIFont(name: "Abuget", size: 100)
+        self.passwordTextField.delegate = self
+        self.emailTextField.delegate = self
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       
     }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return true
+    }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
