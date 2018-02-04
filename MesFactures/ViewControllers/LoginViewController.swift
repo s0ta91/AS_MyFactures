9029c66c685c421acef1cb4938942d5c4bdf1846
Sebastien Constant <constant.sebastien@gmail.com>
2018-02-04 12:50:55 +0100

    Authentification biometrique
    - Shake textField si le mot de passe est faux
    - Ajout du support pour TouchID
    - AJout du support pour FaceID
    - Gestion des cas d’erreurs pour TouchID/FaceID
    ** Affichage du champs password si la vérification TouchID/FaceID est erronée
    ** Affichage du champs password si l’utilisateur annule la vérification TouchID/FaceID

--- MesFactures/ViewControllers/LoginViewController.swift
+++ MesFactures/ViewControllers/LoginViewController.swift
@@ -7,21 +7,35 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class LoginViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var ui_mesfacturesTextField: UITextField!
     @IBOutlet weak var ui_passwordTextField: UITextField!
     @IBOutlet weak var ui_createNewPasswordButton: UIButton!
+    @IBOutlet weak var ui_connexionButton: UIButton!
+    @IBOutlet weak var ui_deletePasswordButton: UIButton!
     
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
         /** DEBUG **/
 //        DbManager().reInitMasterPassword()
         
-        showHideCreateNewPasswordButton()
+        /** Set the font for title **/
         ui_mesfacturesTextField.font = UIFont(name: "Abuget", size: 100)
+        
+        /** Hide 'createNewPasswordButton' if a user password exists in the iPhone Keychain  **/
+        showHideCreateNewPasswordButton()
+        
+        /** Delegation for password textField **/
         self.ui_passwordTextField.delegate = self
     }
     
@@ -39,34 +53,87 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         if let db = DbManager().getDb() {
             if db.hasMasterPassword() == true {
                 ui_createNewPasswordButton.isHidden = true
+                if LAContext().canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: nil) == true {
+                    ui_passwordTextField.isHidden = true
+                    ui_connexionButton.isHidden = true
+                    ui_deletePasswordButton.isHidden = true
+                    unlockWithBiometrics()
+                }
             }else {
                 ui_createNewPasswordButton.isHidden = false
             }
         }
     }
     
-    
-    @IBAction func unlockWithPassword(_ sender: Any) {
-        if let typedPassword = ui_passwordTextField.text,
-            let storedPassword = DbManager().getMasterPassword(),
-            typedPassword == storedPassword {
-            displayGroupTableViewController()
+    private func unlockWithBiometrics () {
+        let context = LAContext()
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: nil)  {
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Déverouiller MesFactures", reply: { (isOwnerConfirmed, authError) in
+                /**
+                    Going back from secondary traitment to firt traitment /
+                    Revenir à l'éxécution du code a premier plan après que le traitement en arrière plan (Identifiaction empreinte ou visage) soit effectuée
+                 **/
+                DispatchQueue.main.async {
+                     /** Unlock application and show group screnn **/
+                    if isOwnerConfirmed == true {
+                        self.displayGroupTableViewController()
+                    }
+                    if let error = authError {
+                        switch error {
+                            case LAError.userCancel:
+                                self.showPasswordField()
+                            case LAError.biometryLockout:
+                                self.showPasswordField()
+                            default :
+                                 self.showPasswordField()
+                        }
+                    }
+                }
+            })
         }
     }
     
-    func displayGroupTableViewController () {
+    private func showPasswordField () {
+        ui_passwordTextField.isHidden = false
+        ui_connexionButton.isHidden = false
+        ui_deletePasswordButton.isHidden = false
+    }
+    
+    private func displayGroupTableViewController () {
         if let GroupTableVC = storyboard?.instantiateViewController(withIdentifier: "GroupTableViewController") as? GroupTableViewController {
-//            show(GroupTableVC, sender: nil)
             GroupTableVC.modalTransitionStyle = .crossDissolve
             present(GroupTableVC, animated: true, completion: nil)
         }
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    private func showAlertMessage (_ errorMessage: String) {
+        let alertBox = UIAlertController(title: "Une erreur est survenue", message: errorMessage, preferredStyle: .alert)
+        alertBox.addAction(UIAlertAction(title: "OK", style: .cancel, handler: { (action: UIAlertAction) in
+            self.showPasswordField()
+        }))
     }
-
-
+    
+    func shakeTextField() {
+        let animation = CABasicAnimation(keyPath: "position")
+        animation.duration = 0.05
+        animation.repeatCount = 5
+        animation.autoreverses = true
+        animation.fromValue = NSValue(cgPoint: CGPoint(x: ui_passwordTextField.center.x - 4, y: ui_passwordTextField.center.y))
+        animation.toValue = NSValue(cgPoint: CGPoint(x: ui_passwordTextField.center.x + 4, y: ui_passwordTextField.center.y))
+        
+        ui_passwordTextField.layer.add(animation, forKey: "position")
+    }
+    
+    @IBAction func unlockWithPassword(_ sender: Any) {
+        if let typedPassword = ui_passwordTextField.text,
+            let storedPassword = DbManager().getMasterPassword() {
+            if typedPassword == storedPassword {
+                displayGroupTableViewController()
+            }else {
+                shakeTextField()
+                ui_passwordTextField.text = ""
+            }
+        }
+    }
 }
 
