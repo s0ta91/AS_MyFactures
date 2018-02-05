5243973a52f22be8d90a537328e6516c796e5659
Sebastien Constant <constant.sebastien@gmail.com>
2018-02-05 19:04:09 +0100

    Finalisation storyboard ‘GroupViewController’
    - Design de la cellule
    - Ajout du header

---
+++ MesFactures/CustomCollectionViewCell/HeaderGroupView.swift
@@ -0,0 +1,14 @@
+//
+//  HeaderGroupCollectionViewCell.swift
+//  MesFactures
+//
+//  Created by Sébastien on 05/02/2018.
+//  Copyright © 2018 Sébastien Constant. All rights reserved.
+//
+
+import UIKit
+
+class HeaderGroupView: UICollectionReusableView {
+    
+    @IBOutlet weak var headerViewLabel: UILabel!
+}
--- MesFactures/ViewControllers/GroupViewController.swift
+++ MesFactures/ViewControllers/GroupViewController.swift
@@ -8,10 +8,10 @@
 
 import UIKit
 
-class GroupViewController: UIViewController, UICollectionViewDataSource {
+class GroupViewController: UIViewController {
   
     @IBOutlet weak var groupCV: UICollectionView!
-    
+    @IBOutlet weak var backView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,16 +24,8 @@ class GroupViewController: UIViewController, UICollectionViewDataSource {
         // Dispose of any resources that can be recreated.
     }
     
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 3
-    }
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell_group = collectionView.dequeueReusableCell(withReuseIdentifier: "cell_group", for: indexPath)
-        return cell_group
-    }
     
-
     /*
     // MARK: - Navigation
 
@@ -44,4 +36,40 @@ class GroupViewController: UIViewController, UICollectionViewDataSource {
     }
     */
 
+}
+
+extension GroupViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        switch kind {
+        case UICollectionElementKindSectionHeader:
+            let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "group_header", for: indexPath) as! HeaderGroupView
+            headerView.headerViewLabel.text = "2018"
+            return headerView
+        default:
+            assert(false, "Unexpected element kind")
+        }
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 12
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell_group = collectionView.dequeueReusableCell(withReuseIdentifier: "cell_group", for: indexPath)
+
+        cell_group.layer.cornerRadius = 8
+        cell_group.layer.borderWidth = 1.0
+        cell_group.layer.borderColor = UIColor.clear.cgColor
+        cell_group.layer.shadowColor = UIColor.lightGray.cgColor
+        cell_group.layer.shadowOffset = CGSize(width:0,height: 2)
+        cell_group.layer.shadowRadius = 2.0
+        cell_group.layer.shadowOpacity = 1.0
+        cell_group.layer.masksToBounds = false;
+        cell_group.layer.shadowPath = UIBezierPath(roundedRect:cell_group.bounds, cornerRadius:cell_group.layer.cornerRadius).cgPath
+        
+        return cell_group
+    }
+    
+    
 }
