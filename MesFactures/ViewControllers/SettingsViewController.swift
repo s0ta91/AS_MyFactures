871cc2569c9145b8c33cacaff1f8db77566a0d05
Sebastien Constant <constant.sebastien@gmail.com>
2018-04-16 10:09:47 +0200

    Update pods
    'RealmSwift', '3.3.2'
    'KeychainAccess', '3.1.1'
    
    et toujours :
    'DZNEmptyDataSet'
    'Buglife', '2.8.0'
    'IQKeyboardManagerSwift', ‘5.0.7’

--- MesFactures/ViewControllers/SettingsViewController.swift
+++ MesFactures/ViewControllers/SettingsViewController.swift
@@ -16,12 +16,14 @@ class SettingsViewController: UIViewController {
     @IBOutlet weak var firstCategoryView: UIView!
     @IBOutlet weak var resetPasswordSubcategoryView: UIView!
     @IBOutlet weak var cgvView: UIView!
-    @IBOutlet weak var contactView: UIView!
+    @IBOutlet weak var aboutView: UIView!
+    @IBOutlet weak var contactButton: UIButton!
     
     
     //MARK: - Controller functions
     override func viewDidLoad() {
         super.viewDidLoad()
+        contactButton.isHidden = true
         navigationController?.navigationBar.tintColor = .white
     }
     override func viewWillAppear(_ animated: Bool) {
@@ -37,7 +39,7 @@ class SettingsViewController: UIViewController {
     func setBottomBorderForViews () {
         let grayColor = UIColor.lightGray
         let viewWidth: CGFloat = 1
-        let viewList = [firstCategoryView,resetPasswordSubcategoryView,cgvView,contactView]
+        let viewList = [firstCategoryView,resetPasswordSubcategoryView,cgvView,aboutView]
         for view in viewList {
             view?.addBottomBorderWithColor(color: grayColor, width: viewWidth)
         }
@@ -51,6 +53,9 @@ class SettingsViewController: UIViewController {
         appearance.barTintColor = UIColor(named: "navBarTint")
         appearance.tintColor = .white
         Buglife.shared().presentReporter()
+    }
+    
+    @IBAction func contactButtonPressed(_ sender: UIButton) {
     }
     
     @IBAction func cancelSettingsVC(_ sender: UIBarButtonItem) {
