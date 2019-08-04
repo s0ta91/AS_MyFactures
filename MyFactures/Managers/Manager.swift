30d388f4eb648282a88c5f2387183c031c2c25bf
Sébastien <sebastien.constant.ext@orange.com>
2019-08-04 19:13:05 +0200

    Fix: Présentation modale Login

--- MyFactures/Managers/Manager.swift
+++ MyFactures/Managers/Manager.swift
@@ -409,6 +409,7 @@ class Manager {
     static func presentLoginScreen(fromViewController vc: UIViewController){
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         guard let loginVC = storyboard.instantiateViewController(withIdentifier: "LoginViewController") as? LoginViewController else { fatalError("Could not load LoginVC") }
+        loginVC.modalPresentationStyle = .fullScreen
         vc.present(loginVC, animated: false, completion: nil)
     }
     
